8ff5918f44d282accf97f233f9c6570f0c42e0bd
searchsakib <searchsakib@gmail.com>
2024-10-25 09:33:45 +0600

    uncommented awiat client.connect()

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 
 const dbConnect = async () => {
   try {
-    // await client.connect();
+    await client.connect();
     console.log("Database Connected!");
   } catch (error) {
     console.log(error.name, error.message);
